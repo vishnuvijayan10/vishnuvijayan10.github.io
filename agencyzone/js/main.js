8da7e9285d0321fed9c5235c98734e254481d63a
vishnuvijayan10 <66822219+vishnuvijayan10@users.noreply.github.com>
2021-04-23 05:14:20 +0530

    Resetting

--- agencyzone/js/main.js
+++
@@ -1,9 +0,0 @@
-let hamburger = document.getElementById('hamburger');
-let navbar = document.querySelector('.navbar-nav');
-
- function toggleNavbar(){
-     navbar.classList.toggle('active');
-     this.classList.toggle('animate');
- }
-
-hamburger.addEventListener('click',toggleNavbar);
